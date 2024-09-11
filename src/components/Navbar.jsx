0fafe18bba6f2e48d97318b2082d20ee5f4401fb
Ebrahimsamad <ebrahim7asn@gmail.com>
2024-09-11 18:02:20 +0300

    Fix responsive bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
-import { FaLinkedin, FaGithub, FaFacebook, FaSignOutAlt } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaFacebook,
+  FaSignOutAlt,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa";
 import { useContext, useState } from "react";
 import { FiLoader } from "react-icons/fi";
 import { AuthContext } from "../context/AuthContext";
@@ -7,6 +14,7 @@ import { AuthContext } from "../context/AuthContext";
 const Navbar = () => {
   const { isAuthenticated, logout, user } = useContext(AuthContext);
   const [showSpinner, setShowSpinner] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -19,6 +27,10 @@ const Navbar = () => {
     }, 1000);
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="fixed top-0 left-0 w-full bg-gray-800 shadow-lg z-50">
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
@@ -26,7 +38,15 @@ const Navbar = () => {
           Ebrahim Samad
         </Link>
 
-        <ul className="flex space-x-8">
+        <button onClick={toggleMenu} className="text-gray-100 md:hidden">
+          {menuOpen ? (
+            <FaTimes className="text-2xl" />
+          ) : (
+            <FaBars className="text-2xl" />
+          )}
+        </button>
+
+        <ul className={`md:flex space-x-8 hidden`}>
           <li>
             <NavLink
               to="/"
@@ -65,7 +85,7 @@ const Navbar = () => {
           </li>
         </ul>
 
-        <div className="flex space-x-6 items-center">
+        <div className="hidden md:flex space-x-6 items-center">
           <a
             href="https://www.linkedin.com/in/ebrahim7asn"
             className="text-gray-100 hover:text-yellow-500 transition duration-300"
@@ -109,6 +129,86 @@ const Navbar = () => {
             </NavLink>
           )}
         </div>
+      </div>
+
+      <div
+        className={`md:hidden ${menuOpen ? "block" : "hidden"} bg-gray-800 p-4`}
+      >
+        <ul className="space-y-4">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-yellow-500 transition duration-300 text-lg font-medium"
+                  : "text-gray-100 hover:text-yellow-500 transition duration-300 text-lg font-medium"
+              }
+              onClick={toggleMenu}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-yellow-500 transition duration-300 text-lg font-medium"
+                  : "text-gray-100 hover:text-yellow-500 transition duration-300 text-lg font-medium"
+              }
+              onClick={toggleMenu}
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className="mt-6 flex space-x-6 items-center">
+          <a
+            href="https://www.linkedin.com/in/ebrahim7asn"
+            className="text-gray-100 hover:text-yellow-500 transition duration-300"
+          >
+            <FaLinkedin className="text-2xl" />
+          </a>
+          <a
+            href="https://github.com/Ebrahimsamad"
+            className="text-gray-100 hover:text-yellow-500 transition duration-300"
+          >
+            <FaGithub className="text-2xl" />
+          </a>
+          <a
+            href="https://www.facebook.com/ebrahim7asn"
+            className="text-gray-100 hover:text-yellow-500 transition duration-300"
+          >
+            <FaFacebook className="text-2xl" />
+          </a>
+        </div>
+
+        {isAuthenticated ? (
+          showSpinner ? (
+            <FiLoader className="ml-6 text-yellow-500 animate-spin text-2xl" />
+          ) : (
+            <div className="mt-6 flex items-center space-x-2">
+              <span className="text-gray-100 font-medium">{user?.name}</span>
+              <button
+                onClick={handleLogout}
+                className="text-gray-100 hover:text-yellow-500 transition duration-300 text-lg font-medium"
+                title="Logout"
+              >
+                <FaSignOutAlt className="text-2xl" />
+              </button>
+            </div>
+          )
+        ) : (
+          <NavLink
+            to="/login"
+            className="mt-6 text-gray-100 hover:text-yellow-500 transition duration-300 text-lg font-medium"
+            onClick={toggleMenu}
+          >
+            Login
+          </NavLink>
+        )}
       </div>
     </nav>
   );
